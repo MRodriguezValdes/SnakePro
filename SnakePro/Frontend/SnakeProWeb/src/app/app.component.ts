155108612beead5a7006ee95c3ac0a338702fd3e
mvidalt <mvidalt@cifpfbmoll.eu>
2024-05-13 23:21:53 +0200

    First visual version

--- SnakePro/Frontend/SnakeProWeb/src/app/app.component.ts
+++ SnakePro/Frontend/SnakeProWeb/src/app/app.component.ts
@@ -9,11 +9,28 @@ import { SnakeComunicationsService } from '../services/snake-comunications.servi
 export class AppComponent implements OnInit {
   title = 'my-app';
 
-  constructor(private snakeComunicationsService: SnakeComunicationsService) { }
+  rows = 20;
+  cols = 40;
+  board: number[][] = [];
+
+  constructor(private snakeComunicationsService: SnakeComunicationsService,) { this.initializeBoard();}
 
   ngOnInit() {
     this.snakeComunicationsService.startConnection().then(() => {
       this.snakeComunicationsService.sendMessage("hola");
     });
   }
+
+
+
+  initializeBoard(): void {
+    for (let i = 0; i < this.rows; i++) {
+      this.board[i] = [];
+      for (let j = 0; j < this.cols; j++) {
+        this.board[i][j] = 0;
+      }
+    }
+  }
+
+
 }
