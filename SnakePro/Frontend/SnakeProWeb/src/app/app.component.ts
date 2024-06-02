9b3201b07c80f037d0b941257d23341e915ccd6f
mvidalt <mvidalt@cifpfbmoll.eu>
2024-06-02 14:57:23 +0200

    Fixing bug display:flex

--- SnakePro/Frontend/SnakeProWeb/src/app/app.component.ts
+++ SnakePro/Frontend/SnakeProWeb/src/app/app.component.ts
@@ -6,8 +6,7 @@ import {HttpClient} from "@angular/common/http";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
