6ab032e677016722073d60d7f4b8e6671d4055b3
mvidalt <mvidalt@cifpfbmoll.eu>
2024-06-03 21:29:19 +0200

    adding first version scoreboards

--- SnakePro/Frontend/SnakeProWeb/src/app/app-routing.module.ts
+++ SnakePro/Frontend/SnakeProWeb/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {LoginComponent} from "./login/login.component";
 import {HomeComponent} from "./home/home.component";
 import {RegisterComponent} from "./register/register.component";
 import {canActivate, redirectUnauthorizedTo} from "@angular/fire/auth-guard";
+import {ScoreboardsComponent} from "./scoreboards/scoreboards.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent, ...canActivate(() => redirectUnauthorizedTo('/login'))},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'scoreboards', component: ScoreboardsComponent}
 ];
 
 @NgModule({
