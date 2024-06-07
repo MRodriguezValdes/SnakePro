07b164e3bb3b751657e9fd6f69b01091ce83c229
mvidalt <mvidalt@cifpfbmoll.eu>
2024-06-07 20:47:27 +0200

    Starting menu v1.0 finished

--- SnakePro/Frontend/SnakeProWeb/src/app/starting-menu/starting-menu.component.ts
+++ SnakePro/Frontend/SnakeProWeb/src/app/starting-menu/starting-menu.component.ts
@@ -1,21 +1,36 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {SnakeCommunicationsService} from "../../services/snake-communications.service";
 
 @Component({
   selector: 'app-starting-menu',
   templateUrl: './starting-menu.component.html',
   styleUrls: ['./starting-menu.component.css']
 })
-export class StartingMenuComponent {
+export class StartingMenuComponent implements OnInit {
   isMenuHidden = false;
 
   @Output() startClicked = new EventEmitter<void>();
   @Output() settingsClicked = new EventEmitter<void>();
+  private hasErrorOccurred: boolean | undefined;
+
+  constructor(private snakeCommunicationsService: SnakeCommunicationsService) { }
+
+
+  ngOnInit() {
+    // Suscríbete al observable y actualiza la variable cuando ocurra un error
+    this.snakeCommunicationsService.errorOccurred.subscribe((error) => {
+      this.hasErrorOccurred = true;
+    });
+  }
 
   closeMenu() {
-    this.isMenuHidden = true;
-    setTimeout(() => {
-      this.startClicked.emit();
-    }, 1000);
+    // Solo procede si no ha ocurrido ningún error
+    if (!this.hasErrorOccurred) {
+      this.isMenuHidden = true;
+      setTimeout(() => {
+        this.startClicked.emit();
+      }, 1000);
+    }
   }
 
   openSettings() {
