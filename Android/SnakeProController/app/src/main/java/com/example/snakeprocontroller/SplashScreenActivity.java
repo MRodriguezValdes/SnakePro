3dcf6e572810e80309a2702646d07e44ff0a9604
mvidalt <mvidalt@cifpfbmoll.eu>
2024-06-10 19:38:36 +0200

    Commenting Android part

--- Android/SnakeProController/app/src/main/java/com/example/snakeprocontroller/SplashScreenActivity.java
+++ Android/SnakeProController/app/src/main/java/com/example/snakeprocontroller/SplashScreenActivity.java
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    // Timeout duration for the splash screen in milliseconds
     private static int SPLASH_SCREEN_TIMEOUT = 3000;
 
     @Override
@@ -19,19 +20,24 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        // Load and start fade-in animation on the entire content view
         Animation fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
         findViewById(android.R.id.content).startAnimation(fadeInAnimation);
 
+        // Find the ImageView for the logo
         ImageView imageLogo = findViewById(R.id.imageLogo);
 
+        // Load and start slide-from-right animation on the logo
         Animation slideFromRightAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_from_right);
-
         imageLogo.startAnimation(slideFromRightAnimation);
 
+        // Delay the transition to MainActivity by SPLASH_SCREEN_TIMEOUT milliseconds
         new Handler().postDelayed(() -> {
+            // Create an intent to start MainActivity
             Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
             startActivity(intent);
 
+            // Finish the SplashScreenActivity so it's removed from the back stack
             finish();
         }, SPLASH_SCREEN_TIMEOUT);
     }
